b03621d102376fef4498d4e1ea122d5ed83c7d5a
Nathan Tangaye Siafa <siafanathan@gmail.com>
2017-10-11 11:48:27 +0000

    update score table seeder

--- database/seeds/ScoreTableSeeder.php
+++ database/seeds/ScoreTableSeeder.php
@@ -5,7 +5,6 @@ use Illuminate\Database\Seeder;
 use App\Term;
 use App\Subject;
 use App\Student;
-
 use App\Score;
 
 
@@ -20,70 +19,70 @@ class ScoreTableSeeder extends Seeder
     {
         //
         //
-        $periodOne = Term::where('name', '1st Period')->first();
+    	$periodOne = Term::where('name', '1st Period')->first();
         $periodTwo = Term::where('name', '2nd Period')->first();
-        $periodthree = Term::where('name', '3rd Period')->first();
+        $periodThere = Term::where('name', '3rd Period')->first();
         $periodFour = Term::where('name', '4th Period')->first();
         
-        $maths = Subject::where('name', 'Mathematics')->first();
-        $biology = Subject::where('name', 'Biology')->first();
-        $physics = Subject::where('name', 'Physics')->first();
-        $geo = Subject::where('name', 'Geography')->first();
+    	$maths = Subject::where('name', 'Mathematics')->first();
+    	$biology = Subject::where('name', 'Biology')->first();
+    	$physics = Subject::where('name', 'Physics')->first();
+    	$geo = Subject::where('name', 'Geography')->first();
 
-        $student_one = DB::table('students')
-            ->select('id', 'grade_id')
-            ->where('student_code', 0001)
-            ->first();
-
-        $student_two = DB::table('students')
-            ->select('id', 'grade_id')
-            ->where('student_code', 0002)
-            ->first();
-
-        $student_three = Student::where('id', 3)->select('id')->first();
+    	$student_one = Student::where('student_code', 0001)->first();
+    	$student_two = Student::where('student_code', 0002)->first();
+    	$student_three = Student::where('student_code', 0003)->first();
 
 
         if(DB::table('scores')->get()->count() == 0){
+            $score = new Score();
+            $score->student_id = $student_one->id;
+            $score->grade_id = $student_one->grade_id;
+            $score->subject_id = $maths->id;
+            $score->term_id = $periodOne->id;
+            $score->score = 90;
+            $score->save();
 
-            DB::table('scores')->insert([
+            $score = new Score();
+            $score->student_id = $student_one->id;
+            $score->grade_id = $student_one->grade_id;
+            $score->subject_id = $maths->id;
+            $score->term_id = $periodFour->id;
+            $score->score = 65;
+            $score->save();
 
-                [
-                    'student_id' => $student_one->id,
-                    'grade_id' => $student_one->grade_id, 
-                    'subject_id' => $maths->id, 
-                    'term_id' => $periodOne->id,
-                    'score' => 90
-                     
-                ],
+            $score = new Score();
+            $score->student_id = $student_one->id;
+            $score->grade_id = $student_one->grade_id;
+            $score->subject_id = $biology->id;
+            $score->term_id = $periodThere->id;
+            $score->score = 80;
+            $score->save();
 
-                [
-                    'student_id' => $student_one->id,
-                    'grade_id' => $student_one->grade_id, 
-                    'subject_id' => $maths->id, 
-                    'term_id' => $periodFour->id,
-                    'score' => 65
-                     
-                ],
+            $score = new Score();
+            $score->student_id = $student_one->id;
+            $score->grade_id = $student_one->grade_id;
+            $score->subject_id = $geo->id;
+            $score->term_id = $periodOne->id;
+            $score->score = 77;
+            $score->save();
 
-                [
-                    'student_id' => $student_one->id,
-                    'grade_id' => $student_one->grade_id, 
-                    'subject_id' => $biology->id, 
-                    'term_id' => $periodthree->id,
-                    'score' => 80
-                     
-                ],
+            $score = new Score();
+            $score->student_id = $student_one->id;
+            $score->grade_id = $student_one->grade_id;
+            $score->subject_id = $physics->id;
+            $score->term_id = $periodFour->id;
+            $score->score = 87;
+            $score->save();
 
-                [
-                    'student_id' => $student_three->id,
-                    'grade_id' => $student_three->grade->id, 
-                    'subject_id' => $geo->id, 
-                    'term_id' => $periodOne->id,
-                    'score' => 77
-                     
-                ]
+            $score = new Score();
+            $score->student_id = $student_one->id;
+            $score->grade_id = $student_one->grade_id;
+            $score->subject_id = $biology->id;
+            $score->term_id = $periodOne->id;
+            $score->score = 100;
+            $score->save();
 
-            ]);
 
         } else { echo "\e[scores table is not empty, therefore not seeding "; }
     }
