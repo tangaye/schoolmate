760472968b1d669c17efa2c878f9acef5d0c6971
Nathan Tangaye Siafa <siafanathan@gmail.com>
2017-10-12 12:05:53 +0000

    updated database.php

--- config/database.php
+++ config/database.php
@@ -48,6 +48,22 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'strict' => true,
+            'engine' => null,
+        ],
+
+
+        'heroku' => [
+            'driver' => 'mysql',
             'host' => env('DB_HOST', $server),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', $db),
