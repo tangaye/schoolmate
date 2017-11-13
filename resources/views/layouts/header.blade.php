c88c8a915d68b740d94f6d06c5576986b9e0e1a2
Nathan Tangaye Siafa <siafanathan@gmail.com>
2017-11-13 10:22:02 +0000

    remove right sidebar component

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -31,10 +31,6 @@
 
         <!-- Authentication Links -->
         @yield('user-logout')
-        <!-- Control Sidebar Toggle Button -->
-        <li>
-          <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-        </li>
       </ul>
     </div>
   </nav>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -25,34 +25,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
   @yield('page-css')
 
   <!-- Theme style -->
-  <link href="{{ asset("/bower_components/AdminLTE/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset("/bower_components/AdminLTE/dist/css/AdminLTE.css")}}" rel="stylesheet" type="text/css" />
 
   <!-- AdminLTE Skins. We have chosen the skin-blue for this starter
         page. However, you can choose any other skin. Make sure you
         apply the skin class to the body tag so the changes take effect.
   -->
-  <link href="{{ asset("/bower_components/AdminLTE/dist/css/skins/_all-skins.min.css")}}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset("/bower_components/AdminLTE/dist/css/skins/skin-black.css")}}" rel="stylesheet" type="text/css" />
 </head>
-<!--
-BODY TAG OPTIONS:
-=================
-Apply one or more of the following classes to get the
-desired effect
-|---------------------------------------------------------|
-| SKINS         | skin-blue                               |
-|               | skin-black                              |
-|               | skin-purple                             |
-|               | skin-yellow                             |
-|               | skin-red                                |
-|               | skin-green                              |
-|---------------------------------------------------------|
-|LAYOUT OPTIONS | fixed                                   |
-|               | layout-boxed                            |
-|               | layout-top-nav                          |
-|               | sidebar-collapse                        |
-|               | sidebar-mini                            |
-|---------------------------------------------------------|
--->
+
 <body class="hold-transition skin-black sidebar-mini">
   <div class="wrapper">
 
